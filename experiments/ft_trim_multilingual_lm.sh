befce8d14fc86a0babd840df97a0d7efc30bbc56
asahi417 <asahi1992ushio@gmail.com>
2023-03-01 11:13:55 +0000

    init

--- experiments/ft_trim_multilingual_lm.sh
+++ experiments/ft_trim_multilingual_lm.sh
@@ -1,121 +1,19 @@
-# Fine-tuning trimmed multilingual LM (mt5 small/base) on QG. Instead of running grid search, we use the optimal
-# configuration used in the original fine-tuning
-
-########
-# BASE #
-########
-SIZE="base"
-LA='ja'
-MODEL="mt5-${SIZE}-trimmed-${LA}-${LA}quad-qg"
-lmqg-train -c "lmqg_output/trim/${MODEL}" -d "lmqg/qg_${LA}quad" -m "asahi417/mt5-${SIZE}-trimmed-${LA}" --low-cpu-mem-usage \
--b 16 -g 4 --lr 0.0001 --label-smoothing 0.0 --random-seed 1 -e 30
-lmqg-eval -m "lmqg_output/trim/${MODEL}/epoch_30" -e "lmqg_output/trim/${MODEL}/epoch_30/eval" --language "${LA}" -d "lmqg/qg_${LA}quad" -i "paragraph_answer" --prediction-aggregation "first" --prediction-level "sentence"
-lmqg-push-to-hf -m "lmqg_output/trim/${MODEL}/epoch_30/" -a "${MODEL}" -o "lmqg"
+# Fine-tuning trimmed multilingual LM (mt5 small/base) on QG
+SIZE="small"
+for LA in 'ja' 'ko' 'ru' 'fr' 'de' 'es' 'it'
+do
+  MODEL="mt5-${SIZE}-trimmed-${LA}-${LA}quad-qg"
+  lmqg-train-search -c "lmqg_output/trimmed/${MODEL}" -d "lmqg/qg_${LA}quad" -m "asahi417/mt5-${SIZE}-trimmed-${LA}" -b 32 -g 2 --lr 1e-04 5e-04 1e-03 --epoch-partial 5 -e 15 --label-smoothing 0 0.15 --language "${LA}" --n-max-config 1
+  lmqg-eval -m "lmqg_output/trimmed/${MODEL}/best_model" -e "lmqg_output/trimmed/${MODEL}/best_model/eval" --language "${LA}" -d "lmqg/qg_${LA}quad" -i "paragraph_answer" --prediction-aggregation "first" --prediction-level "sentence"
+  lmqg-push-to-hf -m "lmqg_output/trimmed/${MODEL}/best_model" -a "${MODEL}" -o "lmqg"
+done
 
 SIZE="base"
-LA='ko'
-MODEL="mt5-${SIZE}-trimmed-${LA}-${LA}quad-qg"
-lmqg-train -c "lmqg_output/trim/${MODEL}" -d "lmqg/qg_${LA}quad" -m "asahi417/mt5-${SIZE}-trimmed-${LA}" --low-cpu-mem-usage \
--b 16 -g 4 --lr 0.0005 --label-smoothing 0.15 --random-seed 1 -e 11
-lmqg-eval -m "lmqg_output/trim/${MODEL}/epoch_11" -e "lmqg_output/trim/${MODEL}/epoch_11/eval" --language "${LA}" -d "lmqg/qg_${LA}quad" -i "paragraph_answer" --prediction-aggregation "first" --prediction-level "sentence"
-lmqg-push-to-hf -m "lmqg_output/trim/${MODEL}/epoch_11" -a "${MODEL}" -o "lmqg"
-
-SIZE="base"
-LA='ru'
-MODEL="mt5-${SIZE}-trimmed-${LA}-${LA}quad-qg"
-lmqg-train -c "lmqg_output/trim/${MODEL}" -d "lmqg/qg_${LA}quad" -m "asahi417/mt5-${SIZE}-trimmed-${LA}" --low-cpu-mem-usage \
--b 8 -g 8 --lr 0.0005 --label-smoothing 0.15 --random-seed 1 -e 16
-lmqg-eval -m "lmqg_output/trim/${MODEL}/epoch_16" -e "lmqg_output/trim/${MODEL}/epoch_16/eval" --language "${LA}" -d "lmqg/qg_${LA}quad" -i "paragraph_answer" --prediction-aggregation "first" --prediction-level "sentence"
-lmqg-push-to-hf -m "lmqg_output/trim/${MODEL}/epoch_16" -a "${MODEL}" -o "lmqg"
-
-SIZE="base"
-LA='fr'
-MODEL="mt5-${SIZE}-trimmed-${LA}-${LA}quad-qg"
-lmqg-train -c "lmqg_output/trim/${MODEL}" -d "lmqg/qg_${LA}quad" -m "asahi417/mt5-${SIZE}-trimmed-${LA}" --low-cpu-mem-usage \
--b 8 -g 8 --lr 0.0001 --label-smoothing 0.15 --random-seed 1 -e 24
-lmqg-eval -m "lmqg_output/trim/${MODEL}/epoch_24" -e "lmqg_output/trim/${MODEL}/epoch_24/eval" --language "${LA}" -d "lmqg/qg_${LA}quad" -i "paragraph_answer" --prediction-aggregation "first" --prediction-level "sentence"
-lmqg-push-to-hf -m "lmqg_output/trim/${MODEL}/epoch_24" -a "${MODEL}" -o "lmqg"
-
-SIZE="base"
-LA='de'
-MODEL="mt5-${SIZE}-trimmed-${LA}-${LA}quad-qg"
-lmqg-train -c "lmqg_output/trim/${MODEL}" -d "lmqg/qg_${LA}quad" -m "asahi417/mt5-${SIZE}-trimmed-${LA}" --low-cpu-mem-usage \
--b 8 -g 8 --lr 0.0005 --label-smoothing 0.15 --random-seed 1 -e 17
-lmqg-eval -m "lmqg_output/trim/${MODEL}/epoch_17" -e "lmqg_output/trim/${MODEL}/epoch_17/eval" --language "${LA}" -d "lmqg/qg_${LA}quad" -i "paragraph_answer" --prediction-aggregation "first" --prediction-level "sentence"
-lmqg-push-to-hf -m "lmqg_output/trim/${MODEL}/epoch_17" -a "${MODEL}" -o "lmqg"
-
-SIZE="base"
-LA='es'
-MODEL="mt5-${SIZE}-trimmed-${LA}-${LA}quad-qg"
-lmqg-train -c "lmqg_output/trim/${MODEL}" -d "lmqg/qg_${LA}quad" -m "asahi417/mt5-${SIZE}-trimmed-${LA}" --low-cpu-mem-usage \
--b 8 -g 8 --lr 0.0005 --label-smoothing 0.15 --random-seed 1 -e 10
-lmqg-eval -m "lmqg_output/trim/${MODEL}/epoch_10" -e "lmqg_output/trim/${MODEL}/epoch_10/eval" --language "${LA}" -d "lmqg/qg_${LA}quad" -i "paragraph_answer" --prediction-aggregation "first" --prediction-level "sentence"
-lmqg-push-to-hf -m "lmqg_output/trim/${MODEL}/epoch_10" -a "${MODEL}" -o "lmqg"
-
-SIZE="base"
-LA='it'
-MODEL="mt5-${SIZE}-trimmed-${LA}-${LA}quad-qg"
-lmqg-train -c "lmqg_output/trim/${MODEL}" -d "lmqg/qg_${LA}quad" -m "asahi417/mt5-${SIZE}-trimmed-${LA}" --low-cpu-mem-usage \
--b 8 -g 8 --lr 0.001 --label-smoothing 0.15 --random-seed 1 -e 11
-lmqg-eval -m "lmqg_output/trim/${MODEL}/epoch_11" -e "lmqg_output/trim/${MODEL}/epoch_11/eval" --language "${LA}" -d "lmqg/qg_${LA}quad" -i "paragraph_answer" --prediction-aggregation "first" --prediction-level "sentence"
-lmqg-push-to-hf -m "lmqg_output/trim/${MODEL}/epoch_11" -a "${MODEL}" -o "lmqg"
-
-
-#########
-# SMALL #
-#########
-SIZE="small"
-LA='ja'
-MODEL="mt5-${SIZE}-trimmed-${LA}-${LA}quad-qg"
-lmqg-train -c "lmqg_output/trim/${MODEL}" -d "lmqg/qg_${LA}quad" -m "asahi417/mt5-${SIZE}-trimmed-${LA}" --low-cpu-mem-usage \
--b 64 -g 1 --lr 0.0005 --label-smoothing 0.0 --random-seed 1 -e 21
-lmqg-eval -m "lmqg_output/trim/${MODEL}/epoch_21" -e "lmqg_output/trim/${MODEL}/epoch_21/eval" --language "${LA}" -d "lmqg/qg_${LA}quad" -i "paragraph_answer" --prediction-aggregation "first" --prediction-level "sentence"
-lmqg-push-to-hf -m "lmqg_output/trim/${MODEL}/epoch_21/" -a "${MODEL}" -o "lmqg"
-
-SIZE="small"
-LA='ko'
-MODEL="mt5-${SIZE}-trimmed-${LA}-${LA}quad-qg"
-lmqg-train -c "lmqg_output/trim/${MODEL}" -d "lmqg/qg_${LA}quad" -m "asahi417/mt5-${SIZE}-trimmed-${LA}" --low-cpu-mem-usage \
--b 64 -g 1 --lr 0.001 --label-smoothing 0.15 --random-seed 1 -e 7
-lmqg-eval -m "lmqg_output/trim/${MODEL}/epoch_7" -e "lmqg_output/trim/${MODEL}/epoch_7/eval" --language "${LA}" -d "lmqg/qg_${LA}quad" -i "paragraph_answer" --prediction-aggregation "first" --prediction-level "sentence"
-lmqg-push-to-hf -m "lmqg_output/trim/${MODEL}/epoch_7" -a "${MODEL}" -o "lmqg"
-
-SIZE="small"
-LA='ru'
-MODEL="mt5-${SIZE}-trimmed-${LA}-${LA}quad-qg"
-lmqg-train -c "lmqg_output/trim/${MODEL}" -d "lmqg/qg_${LA}quad" -m "asahi417/mt5-${SIZE}-trimmed-${LA}" --low-cpu-mem-usage \
--b 32 -g 2 --lr 0.001 --label-smoothing 0.15 --random-seed 1 -e 5
-lmqg-eval -m "lmqg_output/trim/${MODEL}/epoch_5" -e "lmqg_output/trim/${MODEL}/epoch_5/eval" --language "${LA}" -d "lmqg/qg_${LA}quad" -i "paragraph_answer" --prediction-aggregation "first" --prediction-level "sentence"
-lmqg-push-to-hf -m "lmqg_output/trim/${MODEL}/epoch_5" -a "${MODEL}" -o "lmqg"
-
-SIZE="small"
-LA='fr'
-MODEL="mt5-${SIZE}-trimmed-${LA}-${LA}quad-qg"
-lmqg-train -c "lmqg_output/trim/${MODEL}" -d "lmqg/qg_${LA}quad" -m "asahi417/mt5-${SIZE}-trimmed-${LA}" --low-cpu-mem-usage \
--b 32 -g 2 --lr 0.001 --label-smoothing 0.15 --random-seed 1 -e 14
-lmqg-eval -m "lmqg_output/trim/${MODEL}/epoch_14" -e "lmqg_output/trim/${MODEL}/epoch_14/eval" --language "${LA}" -d "lmqg/qg_${LA}quad" -i "paragraph_answer" --prediction-aggregation "first" --prediction-level "sentence"
-lmqg-push-to-hf -m "lmqg_output/trim/${MODEL}/epoch_14" -a "${MODEL}" -o "lmqg"
-
-SIZE="small"
-LA='de'
-MODEL="mt5-${SIZE}-trimmed-${LA}-${LA}quad-qg"
-lmqg-train -c "lmqg_output/trim/${MODEL}" -d "lmqg/qg_${LA}quad" -m "asahi417/mt5-${SIZE}-trimmed-${LA}" --low-cpu-mem-usage \
--b 32 -g 2 --lr 0.001 --label-smoothing 0.15 --random-seed 1 -e 11
-lmqg-eval -m "lmqg_output/trim/${MODEL}/epoch_11" -e "lmqg_output/trim/${MODEL}/epoch_11/eval" --language "${LA}" -d "lmqg/qg_${LA}quad" -i "paragraph_answer" --prediction-aggregation "first" --prediction-level "sentence"
-lmqg-push-to-hf -m "lmqg_output/trim/${MODEL}/epoch_11" -a "${MODEL}" -o "lmqg"
-
-SIZE="small"
-LA='es'
-MODEL="mt5-${SIZE}-trimmed-${LA}-${LA}quad-qg"
-lmqg-train -c "lmqg_output/trim/${MODEL}" -d "lmqg/qg_${LA}quad" -m "asahi417/mt5-${SIZE}-trimmed-${LA}" --low-cpu-mem-usage \
--b 32 -g 2 --lr 0.0005 --label-smoothing 0.15 --random-seed 1 -e 16
-lmqg-eval -m "lmqg_output/trim/${MODEL}/epoch_16" -e "lmqg_output/trim/${MODEL}/epoch_16/eval" --language "${LA}" -d "lmqg/qg_${LA}quad" -i "paragraph_answer" --prediction-aggregation "first" --prediction-level "sentence"
-lmqg-push-to-hf -m "lmqg_output/trim/${MODEL}/epoch_16" -a "${MODEL}" -o "lmqg"
-
-SIZE="small"
-LA='it'
-MODEL="mt5-${SIZE}-trimmed-${LA}-${LA}quad-qg"
-lmqg-train -c "lmqg_output/trim/${MODEL}" -d "lmqg/qg_${LA}quad" -m "asahi417/mt5-${SIZE}-trimmed-${LA}" --low-cpu-mem-usage \
--b 32 -g 2 --lr 0.0005 --label-smoothing 0.0 --random-seed 1 -e 15
-lmqg-eval -m "lmqg_output/trim/${MODEL}/epoch_15" -e "lmqg_output/trim/${MODEL}/epoch_15/eval" --language "${LA}" -d "lmqg/qg_${LA}quad" -i "paragraph_answer" --prediction-aggregation "first" --prediction-level "sentence"
-lmqg-push-to-hf -m "lmqg_output/trim/${MODEL}/epoch_15" -a "${MODEL}" -o "lmqg"
+for LA in 'ja' 'ko' 'ru' 'fr' 'de' 'es' 'it'
+do
+  MODEL="mt5-${SIZE}-trimmed-${LA}-${LA}quad-qg"
+  lmqg-train-search -c "lmqg_output/trimmed/${MODEL}" -d "lmqg/qg_${LA}quad" -m "asahi417/mt5-${SIZE}-trimmed-${LA}" -b 16 -g 4 --lr 1e-04 5e-04 1e-03 --epoch-partial 5 -e 15 --label-smoothing 0 0.15 --language "${LA}" --n-max-config 1
+#  lmqg-train-search -c "lmqg_output/trimmed/${MODEL}" -d "lmqg/qg_${LA}quad" -m "asahi417/mt5-${SIZE}-trimmed-${LA}" -b 32 -g 2 --lr 1e-04 5e-04 1e-03 --epoch-partial 5 -e 15 --label-smoothing 0 0.15 --language "${LA}" --n-max-config 1
+  lmqg-eval -m "lmqg_output/trimmed/${MODEL}/best_model" -e "lmqg_output/trimmed/${MODEL}/best_model/eval" --language "${LA}" -d "lmqg/qg_${LA}quad" -i "paragraph_answer" --prediction-aggregation "first" --prediction-level "sentence"
+  lmqg-push-to-hf -m "lmqg_output/trimmed/${MODEL}/best_model" -a "${MODEL}" -o "lmqg"
+done
---
+++ vocabtrimmer/vocab_refine_on_data.py
@@ -0,0 +1,75 @@
+import json
+import logging
+import os
+from tqdm import tqdm
+from itertools import chain
+import numpy as np
+from transformers import AutoTokenizer
+from datasets import load_dataset
+from vocabtrimmer import character_detector
+from vocabtrimmer.base_trimmer import get_cache_dir
+from collections import defaultdict
+
+
+def get_token_freq(tokens):
+    fq = defaultdict(int)
+    for w in tokens:
+        fq[w] += 1
+    return fq
+
+
+def vocab_refine_on_data(
+        model: str = 'google/mt5-small',
+        language: str = 'ja',
+        dataset: str = 'mc4',
+        dataset_column: str = 'text',
+        dataset_split: str = 'train',
+        dataset_name: str = "ja",
+        chunk: int = 1000,
+        cache_file_vocab: str = None,
+        cache_file_frequency: str = None):
+
+    if cache_file_frequency is None:
+        cache_file_frequency = f"{get_cache_dir('cache')}/frequency.json"
+    if not os.path.exists(cache_file_frequency):
+        os.makedirs(os.path.dirname(cache_file_frequency), exist_ok=True)
+        # processing dataset
+        dataset = load_dataset(dataset, dataset_name, split=dataset_split)
+        logging.info(f'{len(dataset)} texts loaded from {dataset} ({dataset_name})')
+
+        # tokenization
+        if cache_file_vocab is None:
+            cache_file_vocab = f"{get_cache_dir('cache')}/vocab.json"
+        if not os.path.exists(cache_file_vocab):
+            os.makedirs(os.path.dirname(cache_file_vocab), exist_ok=True)
+            logging.info(f"caching all tokens to {cache_file_vocab}")
+            tokenizer = AutoTokenizer.from_pretrained(model)
+            f_writer = open(cache_file_vocab, "w")
+            batch = []
+            for t in tqdm(dataset):
+                batch.append(t[dataset_column])
+                if len(batch) >= chunk:
+                    f_writer.write(json.dumps(get_token_freq(chain(*tokenizer(batch)['input_ids']))) + "\n")
+                    batch = []
+            if len(batch) != 0:
+                f_writer.write(json.dumps(get_token_freq(chain(*tokenizer(batch)['input_ids']))) + "\n")
+            f_writer.close()
+        with open(cache_file_vocab) as f:
+            tokens = f.read().split("\n")
+
+        # count stats
+        logging.info(f"count tokens")
+        unique_token, cnt = np.unique(tokens, return_counts=True)
+        logging.info(f"caching token frequency to {cache_file_frequency}")
+        freq = dict(zip(unique_token.tolist(), cnt.tolist()))
+        with open(cache_file_frequency, "w") as f:
+            json.dump(freq, f)
+
+    with open(cache_file_frequency) as f:
+        freq = json.load(f)
+
+
+if __name__ == '__main__':
+    vocab_refine_on_data(language="ja", dataset_name="ja", cache_file_vocab="mc4/ja_vocab.txt", cache_file_frequency="mc4/ja_frequency.json")
+
+
