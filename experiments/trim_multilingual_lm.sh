1fb2ddfacb154b6cac448acd7f754114476b3e32
asahi417 <asahi1992ushio@gmail.com>
2023-03-01 19:22:41 +0000

    init

--- experiments/trim_multilingual_lm.sh
+++ experiments/trim_multilingual_lm.sh
@@ -21,16 +21,20 @@ done
 # TASK SPECIFIC #
 #################
 # mt5: NEED TO INSTALL `lmqg` https://github.com/asahi417/lm-question-generation
+HF_ORG='vocabtrimmer'
 for LA in 'ja' 'ko' 'ru' 'fr' 'de' 'es' 'it'
 do
   for SIZE in "small" "base"
   do
-    vocabtrimmer-trimming -m "lmqg/mt5-${SIZE}-${LA}quad-qg" -l "${LA}" --repo-id "lmqg/mt5-${SIZE}-${LA}quad-qg-trimmed" -p "ckpts/mt5-${SIZE}-${LA}quad-qg-trimmed" -v 60000
-    git clone "https://huggingface.co/lmqg/mt5-${SIZE}-${LA}quad-qg-trimmed"
-    rm -rf "mt5-${SIZE}-${LA}quad-qg-trimmed/eval"
-    lmqg-eval -m "mt5-${SIZE}-${LA}quad-qg-trimmed" -e "mt5-${SIZE}-${LA}quad-qg-trimmed/eval" --language "${LA}" -d "lmqg/qg_${LA}quad" -i "paragraph_answer"
-    cd "mt5-${SIZE}-${LA}quad-qg-trimmed" && git add . && git commit -m "add eval" && git push && cd ..
-    rm -rf "mt5-${SIZE}-${LA}quad-qg-trimmed"
+    for TARGET in 15000 30000 45000 60000 75000
+    do
+      vocabtrimmer-trimming -m "lmqg/mt5-${SIZE}-${LA}quad-qg" -l "${LA}" --repo-id "${HF_ORG}/mt5-${SIZE}-${LA}quad-qg-trimmed-${TARGET}" -p "ckpts/mt5-${SIZE}-${LA}quad-qg-trimmed" -v ${TARGET}
+      git clone "https://huggingface.co/${HF_ORG}/mt5-${SIZE}-${LA}quad-qg-trimmed"
+      rm -rf "mt5-${SIZE}-${LA}quad-qg-trimmed/eval"
+      lmqg-eval -m "mt5-${SIZE}-${LA}quad-qg-trimmed" -e "mt5-${SIZE}-${LA}quad-qg-trimmed/eval" --language "${LA}" -d "lmqg/qg_${LA}quad" -i "paragraph_answer"
+      cd "mt5-${SIZE}-${LA}quad-qg-trimmed" && git add . && git commit -m "add eval" && git push && cd ..
+      rm -rf "mt5-${SIZE}-${LA}quad-qg-trimmed"
+    done
   done
 done
 
